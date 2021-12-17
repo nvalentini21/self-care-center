cd71744c664bb7fa929106bf47b4ca1dbbb0564c
Nicole Valentini <ncvalentini@gmail.com>
2021-12-16 19:55:44 -0700

    Refactor submit button

--- main.js
+++ main.js
@@ -21,38 +21,38 @@ var errorMessage = document.querySelector('.error-message')
 //Data Variables
 
 var mantras = [
-"I forgive myself and set myself free.",
-"I believe I can be all that I want to be.",
-"I am in the process of becoming the best version of myself",
-"I have the freedom & power to create the life I desire.",
-"I choose to be kind to myself and love myself unconditionally.",
-"My possibilities are endless.",
-"I am worthy of my dreams.",
-"I am enough.",
-"I deserve to be healthy and feel good.",
-"I am full of energy and vitality and my mind is calm and peaceful.",
-"Every day I am getting healthier and stronger.",
-"I honor my body by trusting the signals that it sends me.",
-"I manifest perfect health by making smart choices.",
-"Kulia i ka Nu'u",
+  "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
+  "Don’t let yesterday take up too much of today.",
+  "Every day is a second chance.",
+  "Tell the truth and love everyone.",
+  "I am free from sadness.",
+  "I am enough.",
+  "In the beginning it is you, in the middle it is you and in the end it is you.",
+  "I love myself.",
+  "I am present now.",
+  "Inhale the future, exhale the past.",
+  "This too shall pass.",
+  "Yesterday is not today.",
+  "The only constant is change.",
+  "Onward and upward.",
+  "I am the sky, the rest is weather.",
 ]
 
 var affirmations = [
-"Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
-"Don’t let yesterday take up too much of today.",
-"Every day is a second chance.",
-"Tell the truth and love everyone.",
-"I am free from sadness.",
-"I am enough.",
-"In the beginning it is you, in the middle it is you and in the end it is you.",
-"I love myself.",
-"I am present now.",
-"Inhale the future, exhale the past.",
-"This too shall pass.",
-"Yesterday is not today.",
-"The only constant is change.",
-"Onward and upward.",
-"I am the sky, the rest is weather.",
+  "I forgive myself and set myself free.",
+  "I believe I can be all that I want to be.",
+  "I am in the process of becoming the best version of myself",
+  "I have the freedom & power to create the life I desire.",
+  "I choose to be kind to myself and love myself unconditionally.",
+  "My possibilities are endless.",
+  "I am worthy of my dreams.",
+  "I am enough.",
+  "I deserve to be healthy and feel good.",
+  "I am full of energy and vitality and my mind is calm and peaceful.",
+  "Every day I am getting healthier and stronger.",
+  "I honor my body by trusting the signals that it sends me.",
+  "I manifest perfect health by making smart choices.",
+  "Kulia i ka Nu'u",
 ]
 
 var customMessages = [];
@@ -61,11 +61,9 @@ var customMessages = [];
 
 btnReceiveMessage.addEventListener('click', showMessage);
 btnAddMessage.addEventListener('click', showForm);
-btnSubmit.addEventListener('click', function(){
-  showForm()
-  showCustom()
-  storeCustom()
-})
+btnSubmit.addEventListener('click', storeCustom)
+btnSubmit.addEventListener('click', showCustom)
+
 
 //Functions + Event Handlers
 function getRandomIndex(array) {
@@ -112,9 +110,14 @@ function showCustom(){
 };
 
 function storeCustom(){
+  event.preventDefault();
   if (btnCustomAffRadio.checked === true){
       affirmations.push(inputField.value)
+      console.log(affirmations.length)
+      // return (affirmations)
   } else if (btnCustomManRadio.checked === true){
     mantras.push(inputField.value)
+    console.log(mantras.length)
+    // return (mantras)
   }
 };
